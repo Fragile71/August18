d437fbffc419462f9cd995724bcfdcd0feb70c3a
user <user@DESKTOP-ERR5D88>
2023-08-09 17:29:04 +0900

    August09

---
+++ aug09/src/main/java/com/poseidon/web/dao/BoardDAO.java
@@ -0,0 +1,16 @@
+package com.poseidon.web.dao;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
+
+import com.poseidon.web.dto.BoardDTO;
+
+@Repository
+@Mapper
+public interface BoardDAO {
+
+	 List<BoardDTO> boardList();
+
+}
---
+++ aug09/src/main/java/com/poseidon/web/service/BoardService.java
@@ -0,0 +1,21 @@
+package com.poseidon.web.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.poseidon.web.dao.BoardDAO;
+import com.poseidon.web.dto.BoardDTO;
+
+
+@Service
+public class BoardService {
+@Autowired BoardDAO boardDAO;
+	
+	public List<BoardDTO> boardList() {
+		
+		return boardDAO.boardList();
+	}
+
+}
